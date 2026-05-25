e48c3f05afd8e7a1a6c8f15f4820b19b3cd731c4
8baepp3-baep <8baepp3@gmail.com>
2026-05-25 15:10:58 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,8 @@
 // CONFIGURAÇÃO SUPABASE
 
-const SUPABASE_URL = 'SUA_URL_SUPABASE';
-const SUPABASE_KEY = 'SUA_CHAVE_PUBLICA';
+const SUPABASE_URL = 'https://jnkpwwsygtrhoawzpncn.supabase.co';
+
+const SUPABASE_KEY = 'sb_publishable_ZiKlgiORKDvfTWRBj2T_2Q_WDcdBqaJ';
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
